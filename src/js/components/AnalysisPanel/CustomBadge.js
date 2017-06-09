9158082d5de9251cbedae90962484e1cc7ab38c1
Lucas Cotner <lc07@uw.edu>
2017-06-09 15:41:43 -0400

    Update custom badge to read labels in the current app language

--- src/js/components/AnalysisPanel/CustomBadge.js
+++ src/js/components/AnalysisPanel/CustomBadge.js
@@ -1,13 +1,14 @@
 import React, {PropTypes} from 'react';
 // import text from 'js/languages';
 
-const CustomBadge = (props) => { // (props, context)
-  // const {language} = context;
+const CustomBadge = (props, context) => {
+  const {language} = context;
+  console.log('lang', language);
   return (
     <div className='results__custom-badge'>
-      <div className='results__custom-pre'>{props.chartConfig.totalArea}</div>
+      <div className='results__custom-pre'>{props.chartConfig.totalArea[language]}</div>
       <div className='results__custom-count'>{props.areaHa + ' ha'}</div>
-      <div className='results__custom-active'>{props.chartConfig.dataLabel}</div>
+      <div className='results__custom-active'>{props.chartConfig.dataLabel[language]}</div>
       <div className='results__custom-count'>{props.value}</div>
     </div>
   );
